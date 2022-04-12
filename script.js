497090e0f8e59e066f465e9c2458faf967426b23
kingfeekra <fiachraking@gmail.com>
2022-04-12 19:34:06 +0100

    Create computer play function
    Create function that randomly returns "rock", "paper" or "scissors,
    this represents the computer's play

---
+++ script.js
@@ -0,0 +1,21 @@
+let rock = 1;
+let paper = 2;
+let scissors = 3;
+
+function computerPlay() {
+    let randNumb = Math.floor(Math.random()*3) + 1;
+    console.log(randNumb);
+    if (rock == randNumb) {
+        return "Rock";
+    }
+
+    else if (paper == randNumb) {
+        return "Paper";
+    }
+
+    else {
+        return "Scissors";
+    }
+}
+
+console.log(computerPlay());
