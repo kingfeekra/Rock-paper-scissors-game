447f7510eca152174f71932217d03b4b21062b42
kingfeekra <fiachraking@gmail.com>
2022-04-13 15:53:19 +0100

    Add scissors and paper functionality
    The player can now input rock, paper or scissors to
    the prompt, and the console will log the winner accordingly

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ let rock = 1;
 let paper = 2;
 let scissors = 3;
 
-let playerChoice = prompt("Rock, paper or scissors?");
+let playerSelection = prompt("Rock, paper or scissors?");
 
 function computerPlay() { //function for computer's turn
+    
     let randNumb = Math.floor(Math.random()*3) + 1;
     if (rock == randNumb) {
         console.log("rock");
@@ -28,20 +29,39 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() == "rock" && computerSelection == "paper") {
         return "You lose! Paper beats rock."
     } 
+
+    else if (playerSelection.toLowerCase() == "paper" && computerSelection == "rock") {
+        return "You win! Paper beats rock."
+    }
+
     else if (playerSelection.toLowerCase() == "rock" && computerSelection == "scissors") {
         return "You win! Rock beats scissors."
     }
+
+    else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "rock") {
+        return "You lose! Rock beats scissors."
+    }
+
+    else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "paper") {
+        return "You win! Scissors beats paper."
+    }
+
+    else if (playerSelection.toLowerCase() == "paper" && computerSelection == "scissors") {
+        return "You lose! Paper beats scissors."
+    }
+
+    
     else if (playerSelection.toLowerCase() == computerSelection) {
         return "Draw."
     }
 }
 
-console.log(playRound("rock", computerPlay()));
+console.log(playRound(playerSelection, computerPlay()));
 
 function game() {
     let playerCount = 0;
     let computerCount = 0;
     for (let i = 0; i <  5; i++) {
-        playRound
+        
     }
 }
