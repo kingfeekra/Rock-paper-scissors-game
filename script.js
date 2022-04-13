1442afaf84972d6e09a65878fde030693e542d00
kingfeekra <fiachraking@gmail.com>
2022-04-13 18:12:50 +0100

    Complete game() function
    The game is now functional. 5 rounds of the game are played.
    The user is prompted for an input of rock, paper or scissors 5 times.
    User is now alerted as to whether they won or lost over 5 rounds

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ let rock = 1;
 let paper = 2;
 let scissors = 3;
 
-let playerSelection = prompt("Rock, paper or scissors?");
+//let playerSelection = prompt("Rock, paper or scissors?");
+
+let playerCount = 0;
+let computerCount = 0;
+
+function playerSelect() {
+    return prompt("Rock, paper or scissors?");
+}
 
 function computerPlay() { //function for computer's turn
     
@@ -27,27 +34,33 @@ function computerPlay() { //function for computer's turn
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() == "rock" && computerSelection == "paper") {
+        computerCount++;
         return "You lose! Paper beats rock."
     } 
 
     else if (playerSelection.toLowerCase() == "paper" && computerSelection == "rock") {
+        playerCount++;
         return "You win! Paper beats rock."
     }
 
     else if (playerSelection.toLowerCase() == "rock" && computerSelection == "scissors") {
+        playerCount++;
         return "You win! Rock beats scissors."
     }
 
     else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "rock") {
+        computerCount++;
         return "You lose! Rock beats scissors."
     }
 
     else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "paper") {
+        playerCount++;
         return "You win! Scissors beats paper."
     }
 
     else if (playerSelection.toLowerCase() == "paper" && computerSelection == "scissors") {
-        return "You lose! Paper beats scissors."
+        computerCount++;
+        return "You lose! Scissors beats paper."
     }
 
     
@@ -56,12 +69,25 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-console.log(playRound(playerSelection, computerPlay()));
+//console.log(playRound(playerSelection, computerPlay()));
 
 function game() {
-    let playerCount = 0;
-    let computerCount = 0;
+    
     for (let i = 0; i <  5; i++) {
-        
+        console.log(playRound(playerSelect(), computerPlay()));
+    }
+    if (playerCount > computerCount) {
+        console.log("Winner!");
+        alert("You won!");
+    }
+    else if (computerCount > playerCount) {
+        console.log("Loser.");
+        alert("You lost.")
+    }
+    else {
+        console.log("Game ends in a draw.");
+        alert(("Draw."))  
     }
 }
+
+game();
